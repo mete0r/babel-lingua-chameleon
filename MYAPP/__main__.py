b1dc2a4e2dbf9dfd26cb2449b843d473adfd42fc
mete0r <mete0r@sarangbang.or.kr>
2015-06-24 02:34:10 +0900

    main: remove top-level package name

--- MYAPP/__main__.py
+++ MYAPP/__main__.py
@@ -16,7 +16,7 @@
 #   You should have received a copy of the GNU Affero General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-from MYAPP.cli import main
+from .cli import main
 
 
 if __name__ == '__main__':
