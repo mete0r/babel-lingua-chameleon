055748a62286203e0266a3f5bdbfb7d87ac10268
mete0r <mete0r@sarangbang.or.kr>
2017-03-08 04:48:52 +0900

    docs: repoze.sphinx.autointerface 0.8

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinxarg.ext',
+    'repoze.sphinx.autointerface',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
